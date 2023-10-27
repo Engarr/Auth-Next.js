181586710b856c1aeae14c14f6bfe20917d7467c
Łukasz <lukasz.poczta12@gmail.com>
2023-10-27 13:31:40 +0200

    changed .env

--- app/api/auth/[...nextauth]/option.ts
+++ app/api/auth/[...nextauth]/option.ts
@@ -1,11 +1,27 @@
+import connectToDB from '@/lib/mongodb';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import User from '@/models/user';
 
 export const options = {
   providers: [
     CredentialsProvider({
       name: 'credentials',
       credentials: {},
-      async authorize(credentials, req) {},
+      async authorize(credentials) {
+        const { email, password } = credentials && credentials;
+
+        try {
+          await connectToDB();
+          const user = await User.findOne({ email });
+        } catch (error) {
+          console.log(error);
+        }
+        return null;
+      },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET as string,
+  pages: {
+    signIn: '/',
+  },
 };
--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -2,13 +2,15 @@
 
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
+import type { AppProps } from 'next/app';
 
 type AuthProviderType = {
   children: React.ReactNode;
+  // session: ;
 };
 
-const AuthProvider = ({ children }: AuthProviderType) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const AuthProvider = ({ children, session }: AuthProviderType) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
